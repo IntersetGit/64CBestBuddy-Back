73f739bd63e66d8747df062f54cb14a15b603622
pondkarun <pondkarun@gmail.com>
2021-06-23 01:55:27 +0700

    mas_title_name

---
+++ database/data/mas_title_name.js
@@ -0,0 +1,834 @@
+module.exports = [
+    {
+        id: 100,
+        title_name: "พระมหา"
+    },
+    {
+        id: 101,
+        title_name: "พระอธิการ"
+    },
+    {
+        id: 102,
+        title_name: "พลเรือเอก"
+    },
+    {
+        id: 103,
+        title_name: "พลเรือโท"
+    },
+    {
+        id: 104,
+        title_name: "พลเรือตรี"
+    },
+    {
+        id: 105,
+        title_name: "พลเอก"
+    },
+    {
+        id: 106,
+        title_name: "พลโท"
+    },
+    {
+        id: 107,
+        title_name: "พลตรี"
+    },
+    {
+        id: 108,
+        title_name: "พลทหาร"
+    },
+    {
+        id: 109,
+        title_name: "พลอากาศเอก"
+    },
+    {
+        id: 110,
+        title_name: "พลอากาศโท"
+    },
+    {
+        id: 111,
+        title_name: "พลอากาศตรี"
+    },
+    {
+        id: 112,
+        title_name: "พลอาสาสมัคร"
+    },
+    {
+        id: 113,
+        title_name: "พลตำรวจ"
+    },
+    {
+        id: 114,
+        title_name: "พลทหาร"
+    },
+    {
+        id: 115,
+        title_name: "พลตำรวจสำรองพิเศษ"
+    },
+    {
+        id: 116,
+        title_name: "พลสำรอง"
+    },
+    {
+        id: 117,
+        title_name: "พลอาสาสมัคร"
+    },
+    {
+        id: 118,
+        title_name: "พันเอกพิเศษ"
+    },
+    {
+        id: 119,
+        title_name: "มร."
+    },
+    {
+        id: 120,
+        title_name: "มิสเตอร์"
+    },
+    {
+        id: 121,
+        title_name: "รองศาสตราจารย์"
+    },
+    {
+        id: 122,
+        title_name: "ร้อยเอก"
+    },
+    {
+        id: 123,
+        title_name: "ร้อยโท"
+    },
+    {
+        id: 124,
+        title_name: "ร้อยตรี"
+    },
+    {
+        id: 125,
+        title_name: "ว่าที่ พ.ต.ต."
+    },
+    {
+        id: 126,
+        title_name: "ว่าที่ พ.ต.อ."
+    },
+    {
+        id: 127,
+        title_name: "ว่าที่ ร.ต.ท."
+    },
+    {
+        id: 128,
+        title_name: "ว่าที่ ร.ต.ต."
+    },
+    {
+        id: 129,
+        title_name: "ว่าที่ ร.ต.ท."
+    },
+    {
+        id: 130,
+        title_name: "ว่าที่เรือโท"
+    },
+    {
+        id: 131,
+        title_name: "ว่าที่เรือตรี"
+    },
+    {
+        id: 132,
+        title_name: "ว่าที่นาวาตรี"
+    },
+    {
+        id: 133,
+        title_name: "ว่าที่นาวาตรี"
+    },
+    {
+        id: 134,
+        title_name: "ว่าที่พันตรี"
+    },
+    {
+        id: 135,
+        title_name: "ว่าที่พันตำรวจเอก"
+    },
+    {
+        id: 136,
+        title_name: "ว่าที่พันตำรวจตรี"
+    },
+    {
+        id: 137,
+        title_name: "ว่าที่ร.ต."
+    },
+    {
+        id: 138,
+        title_name: "ว่าที่รท."
+    },
+    {
+        id: 139,
+        title_name: "ว่าที่ร้อยเอก"
+    },
+    {
+        id: 140,
+        title_name: "ว่าที่ร้อยโท"
+    },
+    {
+        id: 141,
+        title_name: "ว่าที่ร้อยตรี"
+    },
+    {
+        id: 142,
+        title_name: "ว่าที่ร้อยตำรวจโท"
+    },
+    {
+        id: 143,
+        title_name: "ว่าที่ร้อยตำรวจตรี"
+    },
+    {
+        id: 144,
+        title_name: "ว่าที่ร.อ.หญิง"
+    },
+    {
+        id: 145,
+        title_name: "ศาสนาจารย์"
+    },
+    {
+        id: 146,
+        title_name: "สิบตำรวจตรี"
+    },
+    {
+        id: 147,
+        title_name: "อาจารย์"
+    },
+    {
+        id: 148,
+        title_name: "แพทย์หญิง"
+    },
+    {
+        id: 149,
+        title_name: "แม่ชี"
+    },
+    {
+        id: 150,
+        title_name: "กำนัน"
+    },
+    {
+        id: 151,
+        title_name: "คุณหญิง"
+    },
+    {
+        id: 152,
+        title_name: "ท่านผู้หญิง"
+    },
+    {
+        id: 153,
+        title_name: "นายแพทย์"
+    },
+    {
+        id: 154,
+        title_name: "นายร้อยตำรวจ"
+    },
+    {
+        id: 155,
+        title_name: "นาวาอากาศ"
+    },
+    {
+        id: 156,
+        title_name: "บริษัท"
+    },
+    {
+        id: 157,
+        title_name: "พันอากาศเอก"
+    },
+    {
+        id: 159,
+        title_name: "ตำรวจ"
+    },
+    {
+        id: 160,
+        title_name: "ว่าที่ ร.ต."
+    },
+    {
+        id: 161,
+        title_name: "สมาชิกโท"
+    },
+    {
+        id: 162,
+        title_name: "สอญ."
+    },
+    {
+        id: 163,
+        title_name: "สัตวแพทย์"
+    },
+    {
+        id: 164,
+        title_name: "สิบตำรวจ"
+    },
+    {
+        id: 165,
+        title_name: "หม่อม"
+    },
+    {
+        id: 166,
+        title_name: "อาสาสมัครทหารพราน"
+    },
+    {
+        id: 167,
+        title_name: "ร.อ.นพ."
+    },
+    {
+        id: 168,
+        title_name: "พระปลัด"
+    },
+    {
+        id: 169,
+        title_name: "สิบเอกหญิง"
+    },
+    {
+        id: 170,
+        title_name: "เรืออากาศโทหญิง"
+    },
+    {
+        id: 171,
+        title_name: "ดาบตำรวจหญิง"
+    },
+    {
+        id: 172,
+        title_name: "จ่าตรีหญิง"
+    },
+    {
+        id: 173,
+        title_name: "จ่าสิบตรีหญิง"
+    },
+    {
+        id: 174,
+        title_name: "จ.ส.ต.หญิง"
+    },
+    {
+        id: 175,
+        title_name: "จ่าสิบเอกหญิง"
+    },
+    {
+        id: 176,
+        title_name: "จ่าสิบโทหญิง"
+    },
+    {
+        id: 177,
+        title_name: "จ.อ.หญิง"
+    },
+    {
+        id: 178,
+        title_name: "จ่าเอกหญิง"
+    },
+    {
+        id: 179,
+        title_name: "ดาบตำรวจตรี"
+    },
+    {
+        id: 180,
+        title_name: "ทันตแพทย์หญิง"
+    },
+    {
+        id: 181,
+        title_name: "นาวาตรีหญิง"
+    },
+    {
+        id: 182,
+        title_name: "น.ต.หญิง"
+    },
+    {
+        id: 183,
+        title_name: "นาวาอากาศหญิง"
+    },
+    {
+        id: 184,
+        title_name: "น.อ.หญิง"
+    },
+    {
+        id: 185,
+        title_name: "น.ท.หญิง"
+    },
+    {
+        id: 186,
+        title_name: "นาวาเอกหญิง"
+    },
+    {
+        id: 187,
+        title_name: "นาวาโทหญิง"
+    },
+    {
+        id: 189,
+        title_name: "พลตรีหญิง"
+    },
+    {
+        id: 190,
+        title_name: "พลตำรวจหญิง"
+    },
+    {
+        id: 191,
+        title_name: "พลฯอาสาหญิง"
+    },
+    {
+        id: 193,
+        title_name: "พลฯตำรวจ"
+    },
+    {
+        id: 194,
+        title_name: "พลฯสำรองพิเศษ"
+    },
+    {
+        id: 195,
+        title_name: "พลฯหญิง"
+    },
+    {
+        id: 196,
+        title_name: "พันจ่าเอกหญิง"
+    },
+    {
+        id: 197,
+        title_name: "พันตรีหญิง"
+    },
+    {
+        id: 198,
+        title_name: "พ.ต.ต.หญิง"
+    },
+    {
+        id: 200,
+        title_name: "พ.ต.อ.หญิง"
+    },
+    {
+        id: 201,
+        title_name: "พันตำรวจโทหญิง"
+    },
+    {
+        id: 202,
+        title_name: "พ.อ.อ.หญิง"
+    },
+    {
+        id: 203,
+        title_name: "พันเอกหญิง"
+    },
+    {
+        id: 204,
+        title_name: "พันโทหญิง"
+    },
+    {
+        id: 205,
+        title_name: "หม่อมราชวงศ์หญิง"
+    },
+    {
+        id: 206,
+        title_name: "รศ.นพ."
+    },
+    {
+        id: 207,
+        title_name: "รศ.พญ."
+    },
+    {
+        id: 208,
+        title_name: "ร้อยตรีหญิง"
+    },
+    {
+        id: 209,
+        title_name: "ร.ต.ต.หญิง"
+    },
+    {
+        id: 210,
+        title_name: "ร.ต.อ.หญิง"
+    },
+    {
+        id: 211,
+        title_name: "ร.ต.ท.หญิง"
+    },
+    {
+        id: 212,
+        title_name: "ร.อ.น.พ."
+    },
+    {
+        id: 213,
+        title_name: "ร้อยเอกหญิง"
+    },
+    {
+        id: 214,
+        title_name: "ร้อยโทหญิง"
+    },
+    {
+        id: 215,
+        title_name: "ว่าที่ร.ท.หญิง"
+    },
+    {
+        id: 216,
+        title_name: "สิบตรีหญิง"
+    },
+    {
+        id: 217,
+        title_name: "ส.ต.ต.หญิง"
+    },
+    {
+        id: 218,
+        title_name: "ส.ต.อ.หญิง"
+    },
+    {
+        id: 219,
+        title_name: "สิบตำรวจโทหญิง"
+    },
+    {
+        id: 220,
+        title_name: "สิบโทหญิง"
+    },
+    {
+        id: 221,
+        title_name: "เรือตรีหญิง"
+    },
+    {
+        id: 222,
+        title_name: "ร.ต.หญิง"
+    },
+    {
+        id: 223,
+        title_name: "ร.อ.หญิง"
+    },
+    {
+        id: 224,
+        title_name: "ร.ท.หญิง"
+    },
+    {
+        id: 225,
+        title_name: "เรือเอกหญิง"
+    },
+    {
+        id: 226,
+        title_name: "เรือโทหญิง"
+    },
+    {
+        id: 227,
+        title_name: "ว่าที่ร.ต.หญิง"
+    },
+    {
+        id: 228,
+        title_name: "จ่าสิบตำรวจตรี"
+    },
+    {
+        id: 229,
+        title_name: "ศ.คุณหญิง"
+    },
+    {
+        id: 230,
+        title_name: "ทพ."
+    },
+    {
+        id: 231,
+        title_name: "นายดาบตำรวจ"
+    },
+    {
+        id: 232,
+        title_name: "จ่าสิบตำรวจโท"
+    },
+    {
+        id: 233,
+        title_name: "พันจ่าตรีหญิง"
+    },
+    {
+        id: 234,
+        title_name: "พลโทหญิง"
+    },
+    {
+        id: 235,
+        title_name: "จ่าโทหญิง"
+    },
+    {
+        id: 236,
+        title_name: "พลเรือตรีหญิง"
+    },
+    {
+        id: 237,
+        title_name: "สมาชิกเอก"
+    },
+    {
+        id: 238,
+        title_name: "รศ.ดร."
+    },
+    {
+        id: 239,
+        title_name: "นาวาเอกพิเศษ"
+    },
+    {
+        id: 28,
+        title_name: "คุณ"
+    },
+    {
+        id: 29,
+        title_name: "เด็กหญิง"
+    },
+    {
+        id: 30,
+        title_name: "เด็กชาย"
+    },
+    {
+        id: 31,
+        title_name: "นางสาว"
+    },
+    {
+        id: 32,
+        title_name: "นาง"
+    },
+    {
+        id: 33,
+        title_name: "นาย"
+    },
+    {
+        id: 34,
+        title_name: "ด๊อกเตอร์"
+    },
+    {
+        id: 35,
+        title_name: "พระสงฆ์"
+    },
+    {
+        id: 36,
+        title_name: "บาทหลวง"
+    },
+    {
+        id: 37,
+        title_name: "หม่อมหลวง"
+    },
+    {
+        id: 38,
+        title_name: "หม่อมราชวงศ์"
+    },
+    {
+        id: 39,
+        title_name: "หม่อมเจ้า"
+    },
+    {
+        id: 40,
+        title_name: "ศาสตราจารย์เกียรติคุณ (กิตติคุณ)"
+    },
+    {
+        id: 41,
+        title_name: "ศาสตราจารย์"
+    },
+    {
+        id: 42,
+        title_name: "ผู้ช่วยศาสตราจารย์"
+    },
+    {
+        id: 43,
+        title_name: "รองศาสตราจารย์"
+    },
+    {
+        id: 44,
+        title_name: "พลตำรวจเอก"
+    },
+    {
+        id: 45,
+        title_name: "พลตำรวจโท"
+    },
+    {
+        id: 46,
+        title_name: "พลตำรวจตรี"
+    },
+    {
+        id: 47,
+        title_name: "พันตำรวจเอก"
+    },
+    {
+        id: 48,
+        title_name: "พันตำรวจโท"
+    },
+    {
+        id: 49,
+        title_name: "พันตำรวจตรี"
+    },
+    {
+        id: 50,
+        title_name: "ร้อยตำรวจเอก"
+    },
+    {
+        id: 51,
+        title_name: "ร้อยตำรวจโท"
+    },
+    {
+        id: 52,
+        title_name: "ร้อยตำรวจตรี"
+    },
+    {
+        id: 53,
+        title_name: "ดาบตำรวจ"
+    },
+    {
+        id: 54,
+        title_name: "จ่าสิบตำรวจ"
+    },
+    {
+        id: 55,
+        title_name: "สิบตำรวจเอก"
+    },
+    {
+        id: 56,
+        title_name: "สิบตำรวจโท"
+    },
+    {
+        id: 57,
+        title_name: "เจ้าอธิการ"
+    },
+    {
+        id: 58,
+        title_name: "เภสัชกร"
+    },
+    {
+        id: 59,
+        title_name: "เรือเอก"
+    },
+    {
+        id: 60,
+        title_name: "เรือโท"
+    },
+    {
+        id: 61,
+        title_name: "เรือตรี"
+    },
+    {
+        id: 62,
+        title_name: "เรืออากาศเอก"
+    },
+    {
+        id: 63,
+        title_name: "เรืออากาศโท"
+    },
+    {
+        id: 64,
+        title_name: "เรืออากาศตรี"
+    },
+    {
+        id: 65,
+        title_name: "จ่าสิบเอกพิเศษ"
+    },
+    {
+        id: 66,
+        title_name: "จ่าสิบเอก"
+    },
+    {
+        id: 67,
+        title_name: "จ่าสิบโท"
+    },
+    {
+        id: 68,
+        title_name: "จ่าสิบตรี"
+    },
+    {
+        id: 69,
+        title_name: "สิบเอก"
+    },
+    {
+        id: 70,
+        title_name: "สิบโท"
+    },
+    {
+        id: 71,
+        title_name: "สิบตรี"
+    },
+    {
+        id: 72,
+        title_name: "จ่าเอก"
+    },
+    {
+        id: 73,
+        title_name: "จ่าโท"
+    },
+    {
+        id: 74,
+        title_name: "จ่าตรี"
+    },
+    {
+        id: 75,
+        title_name: "พันจ่าอากาศเอก"
+    },
+    {
+        id: 76,
+        title_name: "พันจ่าอากาศโท"
+    },
+    {
+        id: 77,
+        title_name: "พันจ่าอากาศตรี"
+    },
+    {
+        id: 78,
+        title_name: "จ่าอากาศเอก"
+    },
+    {
+        id: 79,
+        title_name: "จ่าอากาศโท"
+    },
+    {
+        id: 80,
+        title_name: "จ่าอากาศตรี"
+    },
+    {
+        id: 81,
+        title_name: "นาวาเอก"
+    },
+    {
+        id: 82,
+        title_name: "นาวาโท"
+    },
+    {
+        id: 83,
+        title_name: "นาวาตรี"
+    },
+    {
+        id: 84,
+        title_name: "นาวาอากาศเอก"
+    },
+    {
+        id: 85,
+        title_name: "นาวาอากาศโท"
+    },
+    {
+        id: 86,
+        title_name: "นาวาอากาศตรี"
+    },
+    {
+        id: 87,
+        title_name: "ผศ.ดร."
+    },
+    {
+        id: 88,
+        title_name: "ผศ.นพ."
+    },
+    {
+        id: 89,
+        title_name: "ผู้ใหญ่บ้าน"
+    },
+    {
+        id: 90,
+        title_name: "ทันตแพทย์"
+    },
+    {
+        id: 91,
+        title_name: "พันจ่าเอก"
+    },
+    {
+        id: 92,
+        title_name: "พันจ่าโท"
+    },
+    {
+        id: 93,
+        title_name: "พันจ่าตรี"
+    },
+    {
+        id: 94,
+        title_name: "พันเอก"
+    },
+    {
+        id: 95,
+        title_name: "พันโท"
+    },
+    {
+        id: 96,
+        title_name: "พันตรี"
+    },
+    {
+        id: 97,
+        title_name: "พันตำรวจเอก (พิเศษ)"
+    },
+    {
+        id: 98,
+        title_name: "พระใบฎีกา"
+    },
+    {
+        id: 99,
+        title_name: "พระครู"
+    }
+]
